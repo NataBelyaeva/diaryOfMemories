e433c2da2b07d2f6aa1fa37c6d6b044ae25f573b
NataBelyaeva <123295482+NataBelyaeva@users.noreply.github.com>
2025-05-22 12:03:20 +0700

    Add files via upload

---
+++ app/build/generated/source/buildConfig/release/com/example/diary/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.diary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.diary";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String MAPKIT_API_KEY = "989942ea-ae07-4606-9699-5208d5c0782b";
+}
